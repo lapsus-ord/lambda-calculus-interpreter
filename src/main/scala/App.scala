286b7c07011fda23697da42b72c25ebe27f4c57e
Fernandez Mateo <mateo.fernandez@etu.umonptellier.fr>
2022-03-13 02:06:53 +0100

    Un peu plus contracté

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,21 +1,9 @@
 object App {
 
-  enum Named:
-    case Var(x: String)
-    case Abstraction(x: Named, y: Named)
-    case Application(x: Named, y: Named)
-
-  def toString(expr: Named): String = expr match {
-    case Named.Var(x) => x
-    case Named.Abstraction(x1, x2) => s"λ${toString(x1)}.${toString(x2)}"
-    case Named.Application(x1, x2) => s"(${toString(x1)} ${toString(x2)})"
-    case _ => throw new IllegalArgumentException
-  }
-
   @main def main(): Unit =
     println("--- Lambda Interpreter ---")
     val t1 = Named.Application(Named.Abstraction(Named.Var("x"), Named.Var("y")), Named.Var("y"))
-    println(s"t1 = ${toString(t1)}")
+    println(s"t1 = ${t1.toStr}")
     val t2 =
       Named.Application(
         Named.Abstraction(
@@ -32,6 +20,17 @@ object App {
                 Named.Abstraction(Named.Var("y"), Named.Var("z")),
                 Named.Var("y"))))),
         Named.Application(Named.Var("x"), Named.Var("y")))
-    println(s"t2 = ${toString(t2)}")
+    println(s"t2 = ${t2.toStr}")
+
+  enum Named {
+    def toStr: String = this match
+      case Named.Var(x) => x
+      case Named.Abstraction(x1, x2) => s"λ${x1.toStr}.${x2.toStr}"
+      case Named.Application(x1, x2) => s"(${x1.toStr} ${x2.toStr})"
+
+    case Var(x: String)
+    case Abstraction(x: Named, y: Named)
+    case Application(x: Named, y: Named)
+  }
 
 }
