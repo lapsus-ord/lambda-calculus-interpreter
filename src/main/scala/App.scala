9a5da2f517899b6679dd65d90026d89e719559d1
Fernandez Mateo <mateo.fernandez@etu.umonptellier.fr>
2022-03-13 01:41:23 +0100

    Essai d'une structure pour la représentation d'expression lambdas

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,16 +1,37 @@
 object App {
 
-  enum VarNommees:
-    case LambdaTerme(l: VarNommees)
-    case Argument()
-    case ValeurRetour()
-    case VarExterne(arg: String)
-    case Application(l: VarNommees.LambdaTerme, arg: VarNommees.VarExterne)
+  enum Named:
+    case Var(x: String)
+    case Abstraction(x: Named, y: Named)
+    case Application(x: Named, y: Named)
 
-  def eval(expr: VarNommees) = expr match {
-    case _ => ???
+  def toString(expr: Named): String = expr match {
+    case Named.Var(x) => x
+    case Named.Abstraction(x1, x2) => s"λ${toString(x1)}.${toString(x2)}"
+    case Named.Application(x1, x2) => s"(${toString(x1)} ${toString(x2)})"
+    case _ => throw new IllegalArgumentException
   }
 
-  @main def main = println("hello")
+  @main def main(): Unit =
+    println("--- Lambda Interpreter ---")
+    val t1 = Named.Application(Named.Abstraction(Named.Var("x"), Named.Var("y")), Named.Var("y"))
+    println(s"t1 = ${toString(t1)}")
+    val t2 =
+      Named.Application(
+        Named.Abstraction(
+          Named.Var("x"),
+          Named.Abstraction(
+            Named.Var("y"),
+            Named.Application(
+              Named.Abstraction(
+                Named.Var("z"),
+                Named.Application(
+                  Named.Abstraction(Named.Var("x"), Named.Var("z")),
+                  Named.Var("x"))),
+              Named.Application(
+                Named.Abstraction(Named.Var("y"), Named.Var("z")),
+                Named.Var("y"))))),
+        Named.Application(Named.Var("x"), Named.Var("y")))
+    println(s"t2 = ${toString(t2)}")
 
 }
