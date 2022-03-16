268b00bf43e881bfdc32f19fa3519543d0190e4c
Fernandez Mateo <mateo.fernandez@etu.umontpellier.fr>
2022-03-16 11:47:49 +0100

    Régler le problème de l'application

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,8 +2,9 @@ object App {
 
   @main def main(): Unit =
     println("--- Lambda Interpreter ---")
-    val t1 = Named.App(Named.Abs("x", Named.Var("y")), Named.Var("y"))
+    val t1 = Named.App(Named.Abs("x", Named.Var("x")), Named.Var("x"))
     println(s"t1 = ${t1.toStr}")
+    println(s"t1_bruijn = ${namedToIndex(t1).toStr}")
     val t2 =
       Named.App(
         Named.Abs(
@@ -19,8 +20,8 @@ object App {
         Named.App(Named.Var("x"), Named.Var("y")))
     println(s"t2 = ${t2.toStr}")
     val t3 = DeBruijn.App(
-      DeBruijn.App(DeBruijn.Abs(DeBruijn.Var(0)), DeBruijn.Var(0)),
-      DeBruijn.App(DeBruijn.Abs(DeBruijn.Var(0)), DeBruijn.Var(0)))
+      DeBruijn.App(DeBruijn.Abs(DeBruijn.BoundVar(0)), DeBruijn.BoundVar(0)),
+      DeBruijn.App(DeBruijn.Abs(DeBruijn.BoundVar(0)), DeBruijn.BoundVar(0)))
     println(s"t3 = ${t3.toStr}")
 
   enum Named {
@@ -36,19 +37,28 @@ object App {
 
   enum DeBruijn {
     def toStr: String = this match
-      case DeBruijn.Var(x) => x.toString
+      case DeBruijn.FreeVar(name) => ""
+      case DeBruijn.BoundVar(x) => x.toString
       case DeBruijn.Abs(x) => s"λ ${x.toStr}"
       case DeBruijn.App(x, y) => s"(${x.toStr} ${y.toStr})"
 
-    case Var(x: Int)
+    case FreeVar(name: String)
+    case BoundVar(x: Int)
     case Abs(y: DeBruijn)
     case App(x: DeBruijn, y: DeBruijn)
   }
 
   def namedToIndex(expr: Named): DeBruijn = expr match {
-    case Named.App(x, y) => ???
-    case Named.Abs(x, y) => ???
-    case Named.Var(x) => ???
+    case Named.Var(x) => DeBruijn.FreeVar(x)
+    case Named.Abs(x, y) => aux(y, Map(x -> 0))
+    case Named.App(x, y) => DeBruijn.App(aux(x, Map.empty), aux(x, Map.empty))
+  }
+
+  def aux(expr: Named, map: Map[String, Int]): DeBruijn = expr match {
+    case Named.Var(x) => if map.contains(x) then DeBruijn.Abs(DeBruijn.BoundVar(map(x))) else DeBruijn.FreeVar(x)
+    case Named.Abs(x, y) => aux(y, map.map((key, value) => (key, value + 1)).updated(x, 0))
+    case Named.App(x, y) => DeBruijn.App(aux(x, map.map((key, value) => (key, value + 1))),
+      aux(x, map.map((key, value) => (key, value + 1))))
   }
 
 }
