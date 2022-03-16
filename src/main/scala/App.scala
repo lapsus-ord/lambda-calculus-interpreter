de21de46c924fc208ec34658847dd6e9c9e4e099
Fernandez Mateo <mateo.fernandez@etu.umontpellier.fr>
2022-03-16 10:05:52 +0100

    Ajout indices De Bruijn

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,35 +2,53 @@ object App {
 
   @main def main(): Unit =
     println("--- Lambda Interpreter ---")
-    val t1 = Named.Application(Named.Abstraction(Named.Var("x"), Named.Var("y")), Named.Var("y"))
+    val t1 = Named.App(Named.Abs("x", Named.Var("y")), Named.Var("y"))
     println(s"t1 = ${t1.toStr}")
     val t2 =
-      Named.Application(
-        Named.Abstraction(
-          Named.Var("x"),
-          Named.Abstraction(
-            Named.Var("y"),
-            Named.Application(
-              Named.Abstraction(
-                Named.Var("z"),
-                Named.Application(
-                  Named.Abstraction(Named.Var("x"), Named.Var("z")),
+      Named.App(
+        Named.Abs(
+          "x", Named.Abs(
+            "y", Named.App(
+              Named.Abs(
+                "z", Named.App(
+                  Named.Abs("x", Named.Var("z")),
                   Named.Var("x"))),
-              Named.Application(
-                Named.Abstraction(Named.Var("y"), Named.Var("z")),
+              Named.App(
+                Named.Abs("y", Named.Var("z")),
                 Named.Var("y"))))),
-        Named.Application(Named.Var("x"), Named.Var("y")))
+        Named.App(Named.Var("x"), Named.Var("y")))
     println(s"t2 = ${t2.toStr}")
+    val t3 = DeBruijn.App(
+      DeBruijn.App(DeBruijn.Abs(DeBruijn.Var(0)), DeBruijn.Var(0)),
+      DeBruijn.App(DeBruijn.Abs(DeBruijn.Var(0)), DeBruijn.Var(0)))
+    println(s"t3 = ${t3.toStr}")
 
   enum Named {
     def toStr: String = this match
       case Named.Var(x) => x
-      case Named.Abstraction(x1, x2) => s"λ${x1.toStr}.${x2.toStr}"
-      case Named.Application(x1, x2) => s"(${x1.toStr} ${x2.toStr})"
+      case Named.Abs(x1, x2) => s"λ$x1.${x2.toStr}"
+      case Named.App(x1, x2) => s"(${x1.toStr} ${x2.toStr})"
 
     case Var(x: String)
-    case Abstraction(x: Named, y: Named)
-    case Application(x: Named, y: Named)
+    case Abs(x: String, y: Named)
+    case App(x: Named, y: Named)
+  }
+
+  enum DeBruijn {
+    def toStr: String = this match
+      case DeBruijn.Var(x) => x.toString
+      case DeBruijn.Abs(x) => s"λ ${x.toStr}"
+      case DeBruijn.App(x, y) => s"(${x.toStr} ${y.toStr})"
+
+    case Var(x: Int)
+    case Abs(y: DeBruijn)
+    case App(x: DeBruijn, y: DeBruijn)
+  }
+
+  def namedToIndex(expr: Named): DeBruijn = expr match {
+    case Named.App(x, y) => ???
+    case Named.Abs(x, y) => ???
+    case Named.Var(x) => ???
   }
 
 }
